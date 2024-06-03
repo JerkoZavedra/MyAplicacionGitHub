8af4c1ffd015368855eef35ba73cf9251ca85027
JerkoZavedra <je.zavedra@duocuc.cl>
2024-06-03 09:33:52 -0400

    nuevos cambios en el proyecto

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,6 +4,12 @@ import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';  
+import { MatIconModule } from '@angular/material/icon';  
+
 import { HomePageRoutingModule } from './home-routing.module';
 
 
@@ -12,7 +18,12 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule, 
+    MatIconModule 
   ],
   declarations: [HomePage]
 })
