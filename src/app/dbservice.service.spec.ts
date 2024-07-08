8f9845d3bf678e31ab65317808b4b7bd1822e41a
JerkoZavedra <je.zavedra@duocuc.cl>
2024-07-08 14:29:33 -0400

    ultimos cambios en el proyecto, con incorporacion de carpeta keys

--- src/app/dbservice.service.spec.ts
+++ src/app/dbservice.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DbserviceService } from './dbservice.service';
+import { SQLite } from '@awesome-cordova-plugins/sqlite';
 
 describe('DbserviceService', () => {
   let service: DbserviceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [SQLite]
+    });
     service = TestBed.inject(DbserviceService);
   });
 
